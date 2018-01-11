a46a3959bdd421b5a798dd51c382d31ab347d81a
722C <722C@users.noreply.github.com>
2018-01-11 23:27:57 +0000

    Working.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ else:
     ]
 
 setup(
-    name='diff-match-patch-cython',
+    name='dmp',
     version='1.0.0',
     description = "The Diff Match and Patch libraries offer robust algorithms to perform the operations required for synchronizing plain text.",
     long_description = read('README.rst') + "\n\n" + read("CHANGES.rst"),
