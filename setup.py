f5b2489687a0891e3552cc12eddcca5b73d6433e
722C <722C@users.noreply.github.com>
2018-01-12 00:52:14 +0000

    Working.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup, Extension
-from Cython.Build import cythonize
 
 setup(
     name='dmp',
     version='1.0.0',
-    ext_modules=cythonize('diff_match_patch.pyx'),
+    ext_modules=Extension(
+        'dmp',
+        sources=['diff_match_patch.pyx'],
+    ),
     license="Apache",
     setup_requires=[
         # Setuptools 18.0 properly handles Cython extensions.
