0669a663bd5e058ff9678d3611bbf611586cd2e0
722C <722C@users.noreply.github.com>
2018-01-12 00:19:52 +0000

    Working.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from Cython.Build import cythonize
 setup(
     name='dmp',
     version='1.0.0',
-    packages = ['dmp'],
     ext_modules = cythonize('diff_match_patch.pyx'),
     license = "Apache",
 )
