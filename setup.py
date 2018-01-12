920b89f59fa01882bbb034b21688fb056cd379dc
722C <722C@users.noreply.github.com>
2018-01-12 00:55:28 +0000

    Working.

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup, Extension
 setup(
     name='dmp',
     version='1.0.0',
-    ext_modules=Extension(
-        'dmp',
-        sources=['diff_match_patch.pyx'],
-    ),
+    ext_modules=[
+        Extension(
+            'dmp',
+            sources=['diff_match_patch.pyx'],
+        ),
+    ],
     license="Apache",
     setup_requires=[
         # Setuptools 18.0 properly handles Cython extensions.
