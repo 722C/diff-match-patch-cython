9757eac2852ed279aec74ea06b9cb635dadc857f
722C <722C@users.noreply.github.com>
2018-01-12 00:19:10 +0000

    Working.

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ from Cython.Build import cythonize
 setup(
     name='dmp',
     version='1.0.0',
-    description = "The Diff Match and Patch libraries offer robust algorithms to perform the operations required for synchronizing plain text.",
-    long_description = read('README.rst') + "\n\n" + read("CHANGES.rst"),
     packages = ['dmp'],
-    package_dir = {'': subdir},
     ext_modules = cythonize('diff_match_patch.pyx'),
     license = "Apache",
 )
